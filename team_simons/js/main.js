49b876e0bdaec3b3d8ea897545a6a28f5b73ad8a
janghyeok <pro_hk@naver.com>
2022-03-28 17:54:42 +0900

    로그인까지 완성

--- team_simons/js/main.js
+++ team_simons/js/main.js
@@ -1,37 +1,43 @@
+// 메뉴 클릭 - 숨겨진 메뉴 열기 & 좌측 메뉴 닫기 & 전체화면 블러
 $("#menu").on("click", () => {
   $("#gnb").addClass("on");
   $(".cartBox").removeClass("on");
   $(".closePage").addClass("on");
 });
 
+// 메뉴 닫기 클릭 - 메뉴 닫기 & 전체화면 블러 삭제
 $(".gnbClose").on("click", () => {
   $("#gnb").removeClass("on");
   $(".closePage").removeClass("on");
 });
 
+// 카트 클릭 - 숨겨진 메뉴 열기 & 우측 메뉴 닫기 & 전체화면 블러
 $("#cart").on("click", () => {
   $("#gnb").removeClass("on");
   $(".cartBox").addClass("on");
   $(".closePage").addClass("on");
 });
 
+// 카트 닫기 클릭 - 메뉴 닫기 & 전체화면 블러 삭제
 $(".cartClose").on("click", () => {
   $(".cartBox").removeClass("on");
   $(".closePage").removeClass("on");
 });
 
+// 블러된 화면 누르기 - 좌,우측 메뉴 닫기 & 전체화면 블러 삭제
 $(".closePage").on("click", () => {
   $("#gnb").removeClass("on");
   $(".cartBox").removeClass("on");
   $(".closePage").removeClass("on");
 });
 
-$("#gnb .lnb").on("click", function () {
+// (메뉴) + 표시있는 글 클릭 - + -> - & 다른 li 닫기 & depth02 열기
+$("#gnb .gnbList >li").on("click", function () {
   $(this).toggleClass("on");
-  $(this).siblings().toggleClass("on");
-  $(this).parent().siblings().children().removeClass("on");
+  $(this).siblings().removeClass("on");
 });
 
+// (카트) CART 클릭 - 글자색 변경 & CARTLIST 불러오기
 $(".cartBox .cart").on("click", function () {
   $(this).addClass("on");
   $(this).siblings().removeClass("on");
@@ -39,6 +45,7 @@ $(".cartBox .cart").on("click", function () {
   $(".cartList .recentProduct").removeClass("on");
 });
 
+// (카트) RECENT 클릭 - 글자색 변경 & RECENTLIST 불러오기
 $(".cartBox .recent").on("click", function () {
   $(this).addClass("on");
   $(this).siblings().removeClass("on");
@@ -46,10 +53,12 @@ $(".cartBox .recent").on("click", function () {
   $(".cartList .recentProduct").addClass("on");
 });
 
+// 팝업창 닫기
 $(".popupClose").on("click", () => {
   $("#popup").addClass("off");
 });
 
+// 메인 슬라이더 - loop 적용시 li:nth-child 수정해야됨(각각 클래스명 지정해서 사진 등록)
 const swiper = new Swiper("#mainSlider", {
   autoplay: true,
   //speed: 5000,
@@ -65,4 +74,40 @@ const swiper = new Swiper("#mainSlider", {
   },
 });
 
-$("#main").fullpage({});
+// collection 클릭 - collection 열기
+$("#mainSlider .btnCollection").on("click", () => {
+  $("#mainBanner").addClass("on");
+});
+
+// home 클릭 - home 열기 & footer 닫기
+$("#mainBanner .btnHome").on("click", () => {
+  $("#mainBanner").removeClass("on");
+  $("#footer").removeClass("on");
+});
+
+// 휠 다운 - banner 열기 / footer 열기
+// 휠 업 - footer 닫기 / collection 닫기
+$("#main").on("wheel", (e) => {
+  if (e.originalEvent.wheelDelta < 0) {
+    if ($("#banner").hasClass("on")) {
+      $("#footer").addClass("on");
+    }
+    $("#banner").addClass("on");
+  }
+});
+
+$(".footer").on("wheel", (e) => {
+  if (e.originalEvent.wheelDelta < 0) {
+    $("#footer").addClass("on");
+  }
+});
+
+// footer에 포커스있을 때 휠 업 - footer 닫기 & banner 닫기
+$(".footer").on("wheel", (e) => {
+  if (e.originalEvent.wheelDelta > 0) {
+    if (!$("#footer").hasClass("on")) {
+      $("#banner").removeClass("on");
+    }
+    $("#footer").removeClass("on");
+  }
+});
