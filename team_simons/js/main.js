5b97ad5a10276a4bbd071c5c858924ce0bea158c
janghyeok <pro_hk@naver.com>
2022-03-29 17:29:28 +0900

    고객센터 faq as제외 완성

--- team_simons/js/main.js
+++ team_simons/js/main.js
@@ -74,12 +74,14 @@ const swiper = new Swiper("#mainSlider", {
 // collection 클릭 - collection 열기
 $("#mainSlider .btnCollection").on("click", () => {
   $("#banner").addClass("on");
+  swiper.disable();
 });
 
 // home 클릭 - home 열기 & footer 닫기
 $("#banner .btnHome").on("click", () => {
   $("#banner").removeClass("on");
   $("#footer").removeClass("on");
+  // 재시작 찾아야됨
 });
 
 // 휠 다운 - banner 열기 / footer 열기
@@ -90,22 +92,21 @@ $("#main").on("wheel", (e) => {
       $("#footer").addClass("on");
     }
     $("#banner").addClass("on");
+    swiper.disable();
   }
 });
 
 $(".footer").on("wheel", (e) => {
   if (e.originalEvent.wheelDelta < 0) {
     $("#footer").addClass("on");
-  }
-});
-
-// footer에 포커스있을 때 휠 업 - footer 닫기 & banner 닫기
-$(".footer").on("wheel", (e) => {
-  if (e.originalEvent.wheelDelta > 0) {
-    if (!$("#footer").hasClass("on")) {
-      $("#banner").removeClass("on");
+  } else {
+    if (e.originalEvent.wheelDelta > 0) {
+      if (!$("#footer").hasClass("on")) {
+        $("#banner").removeClass("on");
+      }
+      $("#footer").removeClass("on");
+      // swiper.enable(); 재시작 찾아야됨
     }
-    $("#footer").removeClass("on");
   }
 });
 
@@ -114,13 +115,24 @@ $(".customerList li").on("click", function () {
   $(this).addClass("on").siblings().removeClass("on");
 });
 
-// 고객센터 faqTap 클릭
-$(".customerContents .faqTap label").on("click", function () {
-  $(this).addClass("on").siblings().removeClass("on");
+// 고객센터 faqContents question 클릭
+$(".faqContents .question").on("click", function () {
+  $(this).parent("li").toggleClass("on").siblings().removeClass("on");
 });
 
-// faqMember li 클릭
-$(".faqMember .question").on("click", function () {
-  console.log("hi");
-  $(this).parent("li").toggleClass("on").siblings().removeClass("on");
+// 고객센터 faqTab 클릭
+$(".faqTab label input").on("change", function () {
+  if ($(this).hasClass("member")) {
+    $(".faqMember").addClass("on").siblings().removeClass("on");
+  } else if ($(this).hasClass("product")) {
+    $(".faqProduct").addClass("on").siblings().removeClass("on");
+  } else if ($(this).hasClass("exchange")) {
+    $(".faqExchange").addClass("on").siblings().removeClass("on");
+  } else if ($(this).hasClass("as")) {
+    $(".faqAs").addClass("on").siblings().removeClass("on");
+  } else if ($(this).hasClass("order")) {
+    $(".faqOrder").addClass("on").siblings().removeClass("on");
+  } else {
+    $(".faqEtc").addClass("on").siblings().removeClass("on");
+  }
 });
